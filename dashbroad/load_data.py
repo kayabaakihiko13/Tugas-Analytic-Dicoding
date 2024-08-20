e5bfcd9f25f9b3ba1ad98d864620d2d4080462b5
kayabaakihiko13 <iqbalramad75@gmail.com>
2024-08-20 22:31:36 +0700

    update dashbroad

--- dashbroad/load_data.py
+++ dashbroad/load_data.py
@@ -1,16 +1,40 @@
-import glob
-import os
 import pandas as pd
+import requests
+from io import StringIO
 
 def load_data():
-    FOLDER_PATH = "../PRSA_Data_20130301-20170228/"
-    csv_files = glob.glob(os.path.join(FOLDER_PATH, '*.csv'))
+    BASE_URL = "https://raw.githubusercontent.com/kayabaakihiko13/Tugas-Analytic-Dicoding/main/PRSA_Data_20130301-20170228/"
+    file_names = [
+        "PRSA_Data_Aotizhongxin_20130301-20170228.csv",
+        "PRSA_Data_Changping_20130301-20170228.csv",
+        "PRSA_Data_Dingling_20130301-20170228.csv",
+        "PRSA_Data_Dongsi_20130301-20170228.csv",
+        "PRSA_Data_Guanyuan_20130301-20170228.csv",
+        "PRSA_Data_Gucheng_20130301-20170228.csv",
+        "PRSA_Data_Huairou_20130301-20170228.csv",
+        "PRSA_Data_Nongzhanguan_20130301-20170228.csv",
+        "PRSA_Data_Shunyi_20130301-20170228.csv",
+        "PRSA_Data_Tiantan_20130301-20170228.csv",
+        "PRSA_Data_Wanliu_20130301-20170228.csv",
+        "PRSA_Data_Wanshouxigong_20130301-20170228.csv"
+    ]
 
-    # Periksa apakah ada file yang ditemukan
-    if not csv_files:
-        raise FileNotFoundError(f"Tidak ada file CSV yang ditemukan di folder {FOLDER_PATH}")
+    dataframes = []
 
-    # Jika ada file yang ditemukan, gabungkan mereka menjadi satu DataFrame
-    df_merge = pd.concat([pd.read_csv(file) for file in csv_files], ignore_index=True)
+    for file_name in file_names:
+        url = BASE_URL + file_name
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            csv_content = StringIO(response.text)
+            df = pd.read_csv(csv_content)
+            dataframes.append(df)
+        else:
+            print(f"Failed to retrieve {file_name}")
+
+    if not dataframes:
+        raise ValueError("No CSV files were successfully retrieved")
+
+    df_merge = pd.concat(dataframes, ignore_index=True)
 
     return df_merge
