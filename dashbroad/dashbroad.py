81570880d08d3d91a1db21c5b4c9c1787839d3b2
kayabaakihiko13 <iqbalramad75@gmail.com>
2024-08-20 22:19:48 +0700

    adding load_data

--- dashbroad/dashbroad.py
+++ dashbroad/dashbroad.py
@@ -1,4 +1,4 @@
 import streamlit as st
-
+from load_data import load_data
 st.title("Dashbroad")
-
+st.dataframe(load_data().head())
---
+++ dashbroad/load_data.py
@@ -0,0 +1,16 @@
+import glob
+import os
+import pandas as pd
+
+def load_data():
+    FOLDER_PATH = "../PRSA_Data_20130301-20170228"
+    csv_files = glob.glob(os.path.join(FOLDER_PATH, '*.csv'))
+
+    # Periksa apakah ada file yang ditemukan
+    if not csv_files:
+        raise FileNotFoundError(f"Tidak ada file CSV yang ditemukan di folder {FOLDER_PATH}")
+
+    # Jika ada file yang ditemukan, gabungkan mereka menjadi satu DataFrame
+    df_merge = pd.concat([pd.read_csv(file) for file in csv_files], ignore_index=True)
+
+    return df_merge
